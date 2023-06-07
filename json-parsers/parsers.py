54d447c0011e6256a8300c2fc41f1ecc2d38b160
Wisteli-AdityaJoshi <97009671+Wisteli-AdityaJoshi@users.noreply.github.com>
2023-06-07 12:49:47 +0000

    Update and rename vitals.py to parsers.py

---
+++ json-parsers/parsers.py
@@ -0,0 +1,39 @@
+import json
+import pandas as pd
+
+
+# vitals parser
+def parse_vitals(file): # json file
+    data = file['patientVitals']
+    df = pd.json_normalize(data, record_path=['observationSets', 'observationSegments'], 
+                  meta=['dateOfService', 'recordID', ['observationSets', 'rank'],
+                        ['observationSets', 'setID']])
+    return df
+ 
+    
+# medications parser
+def parse_medications(file): # josn file
+    df = pd.json_normalize(file['patientMedications'])
+    return df
+
+
+# patient demographics parser
+def flatten_demographics(nested_data, parent_key=''): # helper function
+    flatten_data = {}
+    if isinstance(nested_data, dict):
+       for key, value in nested_data.items():
+           new_key = parent_key + '.' + key if parent_key else key
+           flatten_data.update(flatten_demographics(value, new_key))
+    elif isinstance(nested_data, list):
+        for i, item in enumerate(nested_data):
+            new_key = parent_key + '.' + str(i) if parent_key else str(i)
+            flatten_data.update(flatten_demographics(item, new_key))
+    else:
+       flatten_data[parent_key] = nested_data
+    return flatten_data   
+
+def parse_demographics(file): # json file
+    data = file['patientDemographics']
+    flatten_data = flatten_demographics(data)
+    df = pd.DataFrame.from_records([flatten_data])
+    return df
--- json-parsers/vitals.py
+++
@@ -1,8 +0,0 @@
-# function
-def parse_vitals(file): # json file
-    data = file['patientVitals']
-    df = pd.json_normalize(data, record_path=['observationSets', 'observationSegments'], 
-                  meta=['dateOfService', 'recordID', ['observationSets', 'rank'],
-                        ['observationSets', 'setID']])
-    return df
- 
